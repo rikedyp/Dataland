8e1ee2a22386d56c1c8358e9179834660c276c08
rikedyp <rikedyp@users.noreply.github.com>
2017-04-24 02:25:25 +0100

    RikedyGame Engine v0.9 (#3)
    * Add files via upload
    
    Working switchable 2D and 3D game things
    
    * Create about.txt
    
    * Add files via upload

--- GameEngine-Dev/RGameTester.py
+++ GameEngine-Dev/RGameTester.py
@@ -1,262 +1,48 @@
 # RGameTester.py
 # chalkboard program to test the RikedyGame Engine
 from RikedyGame import *
-import numpy as np
-import math
-# set up pygame
-pygame.init()
-pygame.font.init()
-myfont = pygame.font.SysFont('Ariel', 20)
-mainClock = pygame.time.Clock()
-# set up the window
-WINDOWWIDTH = 800   
-WINDOWHEIGHT = 500
-windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))#,pygame.FULLSCREEN)
-pygame.display.set_caption('Graphics1')
-FS = False # Fullscreen toggle
 
-# set up the colors
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-WHITE = (255, 255, 255)
+baselayer = layer(0)
+iframes = []
+nframes = []
+sframes = []
 
-# set up movement variables
-moveLeft = False
-moveRight = False
-moveUp = False
-moveDown = False
-rotUp = False
-rotDown = False
-rotLeft = False
-rotRight = False
-zoomIn = False
-zoomOut = False
-SwitchFocus = False
-RightClickDown = False
-LeftClickDown = False
-xdragfactor = 360/WINDOWWIDTH
-ydragfactor = 360/WINDOWHEIGHT
-zoomfactor = 10
-MOVESPEED = 0.5
-boxindex = 0
-boxmoveindex = 0
+# Import ninja animation
+for i in range(4):
+    path = 'Ninja/4x/idle_%i.png' % (i)
+    ninja = (path, 250)
+    iframes.append(ninja)
+for i in range(6):
+    path = 'Ninja/4x/run_%i.png' % (i)
+    ninja = (path, 100)
+    nframes.append(ninja)
+    path = 'Ninja/4x/swim_%i.png' % (i)
+    ninja = (path, 100)
+    sframes.append(ninja)
+# ninstand = pyganim.PygAnimation(iframes)
+# ninrun = pyganim.PygAnimation(nframes)
+# ninswim = pyganim.PygAnimation(sframes)
 
-# Create some stuff
-Alice = box('Alice','Cuboid',2,2,2,Point3D(0,0,0),WHITE)
-Bob = box('Bob','Cuboid',5,5,5,Point3D(0,0,0),GREEN)
-Corey = box('Corey','Cube',5,5,5,Point3D(0,0,10),[50,100,255])
-Dave = box('Dave','Cube',100,100,100,Point3D(0,0,0),[0,255,255])
-cam = camera(WINDOWWIDTH,WINDOWHEIGHT, Alice.position, 20, 0, 0,1,5)
+g = Game(800,600)
 
-# run the game loop
-Reset = False
-while True:
-  if Reset:
-    cam.r = 20
-    cam.theta = 0
-    cam.phi = 0
+s1 = Scene2D(g)
+Alice = dude('Alice',[0,0],iframes,nframes)
+Bob = dude('Bob',[200,200],iframes,sframes)
+s1.addDude(Alice, 'layer_0')
+s1.addDude(Bob,'layer_0')
+s1.maindude = Alice
+g.addScene(s1)
 
-  # check for events
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      sys.exit()
-   
-    if event.type == pygame.MOUSEBUTTONDOWN:
-      zoomIn = False
-      zoomOut = False
-      xMouse = event.pos[0]
-      yMouse = event.pos[1]
-      if event.button == 1:
-        # left 
-        LeftClickDown = True
-      if event.button == 2:
-        # middle click
-        pass
-      if event.button == 3:
-        # right click
-        RightClickDown = True        
-      if event.button == 4:
-        # scroll no more
-        zoomIn = True
-        zoomOut = False
-        cam.update(SwitchFocus,rotUp,rotDown,rotLeft,rotRight,zoomIn,zoomOut) 
-        zoomIn = False
-      if event.button == 5:
-        zoomIn = False
-        zoomOut = True
-        cam.update(SwitchFocus,rotUp,rotDown,rotLeft,rotRight,zoomIn,zoomOut) 
-        zoomOut = False
+s2 = Scene3D(g)
+Corey = box('Corey', [200,200,0])
+Dave = box('Dave',[200,200,200],Point3D(10,0,0))
+s2.addBox(Corey)
+s2.addBox(Dave)
+s2.movebox = Corey
+s2.focusbox = Dave
+g.addScene(s2)
 
-    if event.type == pygame.MOUSEBUTTONUP:
-      if event.button == 1:
-        # left click up
-        LeftClickDown = False
-        pass
-      if event.button == 2:
-        # middle click up
-        pass
-      if event.button == 3:
-        # right click up
-        RightClickDown = False
+ST = SceneTransferer('ST', s2, [400,200], )
+s1.addSceneTransferer(ST)
 
-    if event.type == pygame.MOUSEMOTION:
-      if RightClickDown:
-        xrel,yrel = event.rel
-        if yrel > 0 and cam.theta > -90:
-          cam.theta -= yrel*ydragfactor
-        if yrel < 0 and cam.theta < 90:
-          cam.theta -= yrel*ydragfactor
-        #if xrel > 0 and cam.phi > -math.pi/2:
-        cam.phi -= xrel*xdragfactor
-        #if xrel < 0 and cam.phi < math.pi/2:
-        #  cam.phi -= xrel/dragfactor
-
-    if event.type == KEYDOWN:
-      # change the keyboard variables
-      if event.key == K_LEFT:
-        rotRight = False
-        rotLeft = True
-      if event.key == K_RIGHT:
-        rotLeft = False
-        rotRight = True
-      if event.key == K_UP:
-        rotDown = False
-        rotUp = True
-      if event.key == K_DOWN:
-        rotUp = False
-        rotDown = True
-      if event.key == ord('x'):
-        zoomIn = True
-        zoomOut = False
-      if event.key == ord('z'):
-      	zoomIn = False
-      	zoomOut = True
-      if event.key == ord('w'):
-        moveUp = True
-        moveDown = False
-      if event.key == ord('s'):
-        moveUp = False
-        moveDown = True
-      if event.key == ord('a'):
-        moveLeft = True
-        moveRight = False
-      if event.key == ord('d'):
-        moveLeft = False
-        moveRight = True
-      if event.key == ord('r'):
-        Reset = True
-      if event.key == ord('q'):
-        boxindex += 1
-        if boxindex >= len(box._boxes):
-          boxindex = 0        
-        cam.focus = box._boxes[boxindex].position
-        print(box._boxes[boxindex]) 
-        print('who is now in focus.')
-        #cam.focus = boxes
-        SwitchFocus = True
-        
-      if event.key == ord('e'):
-        boxmoveindex += 1
-        if boxmoveindex >= len(box._boxes):
-          boxmoveindex = 0
-        print(box._boxes[boxmoveindex])
-        print('who can now be moved')
-
-    if event.type == KEYUP:
-      if event.key == K_ESCAPE:
-        pygame.quit()
-        sys.exit()
-      if event.key == ord('r'):
-        Reset = False
-      if event.key == K_LEFT:
-        rotLeft = False
-      if event.key == K_RIGHT:
-        rotRight = False
-      if event.key == K_UP:
-        rotUp = False
-      if event.key == K_DOWN:
-        rotDown = False
-      if event.key == ord('w'):
-        moveUp = False
-      if event.key == ord('s'):
-        moveDown = False
-      if event.key == ord('a'):
-        moveLeft = False
-      if event.key == ord('d'):
-        moveRight = False
-      if event.key == ord('q'):
-        SwitchFocus = False
-      if event.key == ord('x'):
-        zoomIn = False
-      if event.key == ord('z'):
-        zoomOut = False
-      if event.key == ord('f') and FS == False:
-        FS = True
-        windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT),pygame.FULLSCREEN)
-      elif event.key == ord('f') and FS == True:
-        FS = False
-        windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-      else:
-        pass
-
-  cam.update(SwitchFocus,rotUp,rotDown,rotLeft,rotRight,zoomIn,zoomOut) 
-  #print(Bob.position.x)
-  #print(box._boxes[boxindex].position.x)
-  #print(boxindex)
-  #print(boxmoveindex)
-  
-  windowSurface.fill(BLACK)
-  color = WHITE
-  color = GREEN
-  box._boxes[boxmoveindex].position = box._boxes[boxmoveindex].move(box._boxes[boxmoveindex].position,MOVESPEED,moveUp,moveDown,moveLeft,moveRight)
-  
-  # print(cam.theta,cam.phi,cam.r)
-  # print('ok')
-  # Corey = box('Corey','Bean',3,ss3,3,Point3D(0,0,0))
-
-  # print(box._boxes)
-  # print(' ')
-  # print(box._boxtypes)
-
-  # print('Bob is dead')
-  # print(box._boxes)
-  # print(' ')
-  # print(box._boxtypes)
-# TODO make game loop part of a class?
-#   Collision detection
-#   Path finding
-#   Make boxes easily creatable and destroyable within game loop or whatever
-#   Make objects rotatable in gamespace (not camera space)  
-#   Make text a fct within camera
-#   Make key and mouse press part a fct within RikedyGame
-
-  # Draw on control instructions
-  text = 'Move box: w,a,s,d,             Rotate camera: up,down,left,right, right click and drag   '
-  textsurface = myfont.render(text, True, (200, 200, 200))
-  windowSurface.blit(textsurface,(50,50))
-  text = 'Reset camera: r,                     Zoom camera: z,x,scroll'
-  textsurface = myfont.render(text, True, (200, 200, 200))
-  windowSurface.blit(textsurface,(50,70))
-  text = 'Switch camera focus: q,              Switch moveable box: e'
-  textsurface = myfont.render(text, True, (200, 200, 200))
-  windowSurface.blit(textsurface,(50,90))
-  text = "Moveable %s: %s" % (box._boxes[boxmoveindex].species,box._boxes[boxmoveindex].name)
-  textsurface = myfont.render(text, True, (200, 200, 200))
-  windowSurface.blit(textsurface,(50,110))
-  text = "%s in focus: %s" % (box._boxes[boxindex].species,box._boxes[boxindex].name)
-  textsurface = myfont.render(text, True, (200, 200, 200))
-  windowSurface.blit(textsurface,(50,130))
-  text = "Toggle Fullscreen: f"
-  textsurface = myfont.render(text, True, (200, 200, 200))
-  windowSurface.blit(textsurface,(50,150))
-  #print(cam.pos.z - Alice.position.z)#38
-  #print(viewerdist)
-  for b in range(len(box._boxes)):
-    box._boxes[b].updatePos(box._boxes[b].position)
-    cam.drawit(box._boxes[b],windowSurface,box._boxes[b].colour)
-  pygame.display.flip()
-  mainClock.tick(40)
-
-
-
+g.play()
--- GameEngine-Dev/RikedyGame.py
+++ GameEngine-Dev/RikedyGame.py
@@ -1,219 +1,66 @@
 # RikedyGame.py
-# RikedyP Game Engine - hopefully a fairly light, simple to understand and easy to implement 2D and pseudo-3D game engine for pygame python3
-# This document uses 2 spaces per indentation - do not use tab
+# RikedyP Game Engine - hopefully a fairly light,
+# simple to understand and easy to implement 2D and
+# pseudo-3D game engine for pygame python3
+# This document adheres to pylint somewhat:
+
+
+# ------------------------ TO DO -------------------------
+
+# Use Point23D.addXYZ more
+# Streamline functions and methods a bit
+# Utilist pygame.sprite stuff
+# Easy to create scenes i.e. game levels - choose 2d or 3d
+# Scenes contain control parameters
+# only objects in the same layer can collide
+# Better variables
+# Maybe use OpenGL for 3D so it can be expanded
+# Set controls within game menu
+# Use bluetooth controller option
+# Do 3D scenes
+
+# ---------------------- - METHOD -------------------------
+
+# Make a game object
+# Create scenes and scene progression stuff
+# game.play()
+
+# ---------------------------------------------------------
+
 import pygame, sys, random, math
-from pygame.locals import *
-import numpy as np
+from pygame.locals import * # what does this do?
+#from itertools import cycle
+from R2D import *
+from R3D import *
 
-class Point3D:
-  def __init__(self, x = 0, y = 0, z = 0):
-    self.x, self.y, self.z = float(x), float(y), float(z)
+class Game(object):
+    # Handles gameplay
 
-  def addX(self,newpos):
-    x = self.x + newpos
-    return Point3D(x,self.y,self.z)
+    def __init__(self,WindowWidth,WindowHeight,caption='RikedyGame Game'):
+        self.WindowHeight = WindowHeight
+        self.WindowWidth = WindowWidth
+        # set up pygame
+        pygame.init()
+        pygame.font.init()
+        self.myfont = pygame.font.SysFont('Ariel', 20)
+        self.mainClock = pygame.time.Clock()
+        # set up the window
+        self.windowSurface = pygame.display.set_mode((WindowWidth, WindowHeight))#,pygame.FULLSCREEN)
+        pygame.display.set_caption(caption)
+        self.scenes = dict()
 
-  def addY(self,newpos):
-    y = self.y + newpos
-    return Point3D(self.x,y,self.z)
+    def addScene(self, scene):
+        self.scenes[scene.name] = scene
 
-  def addZ(self,newpos):
-    z = self.z + newpos
-    return Point3D(self.x,self.y,z)
-
- 
-  def rotateX(self, angle):
-    #  """ Rotates the point around the X axis by the given angle in degrees. """
-    rad = angle * math.pi / 180
-    cosa = math.cos(rad)
-    sina = math.sin(rad)
-    y = self.y * cosa - self.z * sina
-    z = self.y * sina + self.z * cosa
-    return Point3D(self.x, y, z)
- 
-  def rotateY(self, angle):
-    #""" Rotates the point around the Y axis by the given angle in degrees. """
-    rad = angle * math.pi / 180
-    cosa = math.cos(rad)
-    sina = math.sin(rad)
-    z = self.z * cosa - self.x * sina
-    x = self.z * sina + self.x * cosa
-    return Point3D(x, self.y, z)
- 
-  def rotateZ(self, angle):
-     #  """ Rotates the point around the Z axis by the given angle in degrees. """
-    rad = angle * math.pi / 180
-    cosa = math.cos(rad)
-    sina = math.sin(rad)
-    x = self.x * cosa - self.y * sina
-    y = self.x * sina + self.y * cosa
-    return Point3D(x, y, self.z)
- 
-  def project(self, win_width, win_height, fov, viewer_distance):
-    #    """ Transforms this 3D point to 2D using a perspective projection. """
-    factor = fov / (viewer_distance)
-    x = self.x * factor + win_width / 2
-    y = -self.y * factor + win_height / 2
-    return Point3D(x, y, self.z)
-
-# Define the box class - these will be containers which hold images and can move them around in a 3D space
-class box(object):
-  _boxindex = 0
-  _boxes = []
-  _boxtypes = []
-  def __init__(self, name = 'Mr. Default', species = 'Cube',width = 5,height = 5,depth = 5,position = Point3D(0,0,0),colour = [255,255,255]):
-    self.name = name
-    self.species = species # e.g. sprite, goody, baddy, wall, cup, hero, frog
-    self.width = width
-    self.height = height
-    self.depth = depth
-    self.position = self.move(position,0,False,False,False,False)
-    self.verts = self.updatePos(self.position)
-    self._boxes.append(self)
-    self._boxtypes.append(species)
-    self._boxindex += 1
-    self.colour = colour
-
-  def __del__(self):
-  	n = box._boxes.index(self)
-  	del box._boxes[n]
-  	del box._boxtypes[n]
-
-  def updatePos(self,position = Point3D(0,0,0)):
-    # Turn location arguments into a vector array
-
-    x0 = position.x-(self.width/2)
-    x1 = position.x+(self.width/2)
-    y0 = position.y-(self.height/2)
-    y1 = position.y+(self.height/2)
-    z0 = position.z-(self.depth/2)
-    z1 = position.z+(self.depth/2)
-    #print(position.x,position.y,position.z)
-    self.verts = [
-      Point3D(x0,y0,z0),
-      Point3D(x0,y0,z1),
-      Point3D(x0,y1,z0),
-      Point3D(x0,y1,z1),
-      Point3D(x1,y0,z0),
-      Point3D(x1,y0,z1),
-      Point3D(x1,y1,z0),
-      Point3D(x1,y1,z1)
-        ]
-    return self.verts
-
-  def getName(self):
-    return self.name
-
-  def getSpecies(self):
-    return self.species
-
-  def __str__(self):
-    return "%s is a %s of size [%f,%f,%f]" % (self.name, self.species,self.width,self.height,self.depth)
-
-  def move(self,position,speed,moveUp,moveDown,moveLeft,moveRight):
-    if moveUp:
-      position.z += speed
-    if moveDown:
-      position.z -= speed
-    if moveLeft:
-      position.x -= speed
-    if moveRight:
-      position.x += speed
-    return position 
-  
-# Define the camera class which handles drawing the objects
-class camera(object):
-
-  def __init__(self, width, height, focus, r, theta, phi, zoomspeed, rotspeed):
-    self.width = width
-    self.height = height
-    self.focus = focus
-    self.r = r # distance from focus
-    self.theta = theta # altitude angle in radians
-    self.phi = phi # azimuthal angle in radians
-    self.cx = width/2
-    self.cy = height/2
-    self.zoomspeed = zoomspeed
-    self.rotspeed = rotspeed
-    self.pos = Point3D(0,0,r)
-    BLACK = [0,0,0]
-    WHITE = [255,255,255]
-    
-  def __str__(self):
-  	return "This is a camera"
-
-  def update(self,SwitchFocus,rotUp,rotDown,rotLeft,rotRight,zoomIn,zoomOut):
-    if rotDown:
-  	  self.theta -= self.rotspeed
-    if rotUp:
-  	  self.theta += self.rotspeed
-    if rotLeft: 
-      self.phi -= self.rotspeed
-    if rotRight:
-      self.phi += self.rotspeed
-    if zoomIn and self.r > 3:
-      self.r -= self.zoomspeed
-    if zoomOut and self.r < 10000:
-      self.r += self.zoomspeed
-    if SwitchFocus:
-      pass
-
-  def drawit(self,thing,Surface,color):
-#-----------------------------------------------------
-  # vector for transformed vertices
-    t = []
-    vd = []
-    for v in thing.verts:
-      # adjust verts for camera focus
-      d = v.addX(-self.focus.x).addY(-self.focus.y).addZ(-self.focus.z)
-      # Do camera rotations
-      r = d.rotateX(self.theta).rotateY(self.phi)
-      # calculate distance from camera to thing
-      th = self.theta*math.pi/180
-      ph = self.phi*math.pi/180
-      
-      xc = self.r*math.cos(th)*math.sin(ph)
-      yc = self.r*math.sin(th)*math.sin(ph)
-      zc = self.r*math.cos(th)*math.cos(ph)
-      self.pos = Point3D(xc,yc,zc)
-      self.posrot = self.pos.rotateX(self.theta).rotateY(self.phi)
-      xd = r.x + self.posrot.x 
-      yd = r.y + self.posrot.y 
-      zd = r.z + self.posrot.z 
-      camr = np.sqrt(xd*xd+yd*yd+zd*zd)
-      viewer_distance = self.r+r.z 
-      vd.append(float(viewer_distance))
-      if viewer_distance > 0:
-        p = r.project(Surface.get_width(), Surface.get_height(), 512, viewer_distance)
-      #else p = null
-      # Put the point in the list of transformed vertices
-        t.append(p)
-    vd = sorted(vd)
-    if vd[0] < 0.8:
-      t = 'null'
+    def play(self):
+        scenelist = cycle(self.scenes)
+        while True:
+            scene = next(scenelist)
+            self.scenes[scene].play(self.windowSurface)
+        # for scene in self.scenes:
+        #     self.scenes[scene].play(self.windowSurface)
+        # Do a next_scene variable
+        # Make scenes easily switchable - can trigger new scene from old scene and return to old scene in prior state
 
 
-  # Calculate the average Z values of each face.
-  # avg_z = []
-  # i = 0
-  # for f in self.faces:
-  #   z = (t[f[0]].z + t[f[1]].z + t[f[2]].z + t[f[3]].z) / 4.0
-  #   avg_z.append([i,z])
-  #   i = i + 1
-#-------------------------------------------------
-    pointlist = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]]
-    if t == 'null':
-      return viewer_distance
-    for i in range(len(t)):
-      xx = int(t[i].x)
-      yy = int(t[i].y)
-      pointlist[i] = [xx,yy]
-      pygame.draw.circle(Surface,color,(xx,yy),3)
-      
-    closed = True
-    #pointlist = sorted(pointlist)
-    pygame.draw.aalines(Surface,color,closed,pointlist,1)
-    return viewer_distance
 
-class Game():
-  pass
